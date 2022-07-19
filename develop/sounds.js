82e6debdea93debe2690b91f0613d8954e9e7627
Amber Zimmerman <ajz1221@gmail.com>
2022-07-18 18:38:32 -0600

    adding the end of a transition

--- develop/sounds.js
+++ develop/sounds.js
@@ -1,19 +1,30 @@
-// When I press a keyboard key the specific screen button is targeted
-// When I press a keyboard key, the button enlarges and the outerborder glows
-// When I press a keyboard key, I am able to play the sound
-// When I press a key repeatedly, there is no delay in the sound
+function makeNoise(e) {
+  // This is how we would use an attribute selector to select one of the data keys
+  // const audio = document.querySelector('audio[data-key=65')
 
-function makeNoise() {
-  let key = document.querySelector(".key");
-  key.classList.add(".pressed");
+  // We are going to change the variable audio above to one that select what is being passed through the event by using back ticks and es6 template literal instead
+  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  // When I press a keyboard key, I am able to play the sound
+  if (!audio) return;
+
+  // When I press a key repeatedly, there is no delay in the sound
+  audio.currentTime = 0;
+  audio.play();
+
+  // When I press a keyboard key, the button enlarges and the outerborder glows
+  key.classList.add("pressed");
 }
 
-document.addEventListener("keydown", function (e) {
-  console.log(e);
-});
-
-function test() {
-  console.log("this is working");
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("pressed");
+  console.log(this);
 }
 
-test();
+let keys = document.querySelectorAll(`.key`);
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+// When I press a keyboard key the specific keycode is targeted
+document.addEventListener("keydown", makeNoise);
